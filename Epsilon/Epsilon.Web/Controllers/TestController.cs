108129aad9c0ac1a27177005db76e5f8d52c1a40
Panos <codiply@gmail.com>
2015-06-17 22:54:43 +0100

    Added support for alerts.

---
+++ Epsilon/Epsilon.Web/Controllers/TestController.cs
@@ -0,0 +1,51 @@
+﻿using Epsilon.Logic.SqlContext;
+using Epsilon.Web.Controllers.BaseControllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Epsilon.Web.Controllers
+{
+    public class TestController : AnonymousBaseController
+    {
+        private readonly IEpsilonContext _dbContext;
+
+        public TestController(IEpsilonContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public ActionResult CreateDatabase()
+        {
+            var x = _dbContext.Users.Any();
+            return Content(x.ToString());
+        }
+
+        public ActionResult SuccessAlert()
+        {
+            Success("This is a <b>Success</b> alert.", true);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult WarningAlert()
+        {
+            Warning("This is a <b>Warning</b> alert.", true);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult InformationAlert()
+        {
+            Warning("This is an <b>Information</b> alert.", true);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult DangerAlert()
+        {
+            Danger("This is a <b>Danger</b> alert.", true);
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
