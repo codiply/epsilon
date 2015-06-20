ff05796bb98a3bafee1d885eb5da8c72a0c2fdeb
Panos <codiply@gmail.com>
2015-06-20 19:36:53 +0100

    Installed T4TS.

---
+++ Epsilon/Epsilon.Web/Controllers/ApiControllers/AddressController.cs
@@ -0,0 +1,14 @@
+﻿using Epsilon.Web.Controllers.BaseControllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Epsilon.Web.Controllers.ApiControllers
+{
+    public class AddressController : BaseApiController
+    {
+    }
+}
