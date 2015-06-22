a9f727a7e0acda129fe9899846cfca40bb59a0c2
Panos <codiply@gmail.com>
2015-06-22 22:47:42 +0100

    Added index to AdminController. Added page with cache keys and Clear Cache button.

--- Epsilon/Epsilon.Web/Controllers/AdminController.cs
+++ Epsilon/Epsilon.Web/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Epsilon.Logic.Constants;
+using Epsilon.Logic.Infrastructure.Interfaces;
 using Epsilon.Web.Controllers.BaseControllers;
 using System;
 using System.Collections.Generic;
@@ -11,9 +12,35 @@ namespace Epsilon.Web.Controllers
     [Authorize(Roles = AspNetRoles.Admin)]
     public class AdminController : BaseController
     {
+        private IAppCache _appCache;
+
+        public AdminController(
+            IAppCache appCache)
+        {
+            _appCache = appCache;
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult AppCacheKeys()
+        {
+            var model = _appCache.AllKeys().OrderBy(x => x).ToList();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ClearAppCache()
+        {
+            _appCache.Clear();
+
+            Success("App Cache has been cleared!");
+
+            return RedirectToAction("AppCacheKeys");
         }
     }
 }
