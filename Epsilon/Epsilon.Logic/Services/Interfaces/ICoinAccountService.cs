d693da9b09633d8150fb2f445f00ab2e79f18834
Panos <codiply@gmail.com>
2015-07-03 21:16:21 +0100

    Some clarifying comments on MakeTransaction.

--- Epsilon/Epsilon.Logic/Services/Interfaces/ICoinAccountService.cs
+++ Epsilon/Epsilon.Logic/Services/Interfaces/ICoinAccountService.cs
@@ -13,6 +13,22 @@ namespace Epsilon.Logic.Services.Interfaces
     {
         Task CreateAccount(string accountId);
 
+        /// <summary>
+        /// It makes a transaction for the specific account.
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <param name="amount">
+        /// Positive (credits the account) or negative (debits the account). The amount will be simply added to the balance.
+        /// Zero is also possible in order to record a transaction that does not change the balance.</param>
+        /// <param name="transactionTypeId">
+        /// The transaction type Id. This is purely for recording it on the transaction and it not used in any other way.
+        /// This means it is not used to double check the amount has the right sign in agreement with the transaction type.
+        /// </param>
+        /// <param name="reference">
+        /// A reference that might have different meanings depending on the transaction type.
+        /// Usually it will be the Id of an Entity related to the specific transaction type.
+        /// </param>
+        /// <returns></returns>
         Task<CoinAccountTransactionStatus> MakeTransaction(
             string accountId, 
             Decimal amount, 
