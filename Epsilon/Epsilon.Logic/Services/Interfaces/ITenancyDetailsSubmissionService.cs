ea8b65d41ba7416d5cbb3821e57ad868355f91fc
Panos <codiply@gmail.com>
2015-07-24 16:24:38 +0100

    Added api controller for getting info about user submissions.

--- Epsilon/Epsilon.Logic/Services/Interfaces/ITenancyDetailsSubmissionService.cs
+++ Epsilon/Epsilon.Logic/Services/Interfaces/ITenancyDetailsSubmissionService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Epsilon.Logic.JsonModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,5 +21,7 @@ namespace Epsilon.Logic.Services.Interfaces
             string userIpAddress,
             Guid submissionUniqueId,
             Guid addressUniqueId);
+
+        Task<UserTenancyDetailsSubmissionInfo> GetUserSubmissionInfo(string userId);
     }
 }
---
+++ Epsilon/Epsilon.Web/Controllers/ApiControllers/SubmissionController.cs
@@ -0,0 +1,30 @@
+﻿using Epsilon.Logic.JsonModels;
+using Epsilon.Logic.Services.Interfaces;
+using Epsilon.Web.Controllers.BaseControllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Epsilon.Web.Controllers.ApiControllers
+{
+    public class SubmissionController : BaseApiController
+    {
+        private readonly ITenancyDetailsSubmissionService _tenancyDetailsSubmissionService;
+
+        public SubmissionController(
+            ITenancyDetailsSubmissionService tenancyDetailsSubmissionService)
+        {
+            _tenancyDetailsSubmissionService = tenancyDetailsSubmissionService;
+        }
+
+        [HttpGet]
+        public async Task<UserTenancyDetailsSubmissionInfo> MySubmissionInfo()
+        {
+            return await _tenancyDetailsSubmissionService.GetUserSubmissionInfo(GetUserId());
+        }
+    }
+}
