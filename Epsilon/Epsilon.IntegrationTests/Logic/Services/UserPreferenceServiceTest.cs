e2d8891f0b9dd4798e681bd0b24a6660da810ef6
Panos <codiply@gmail.com>
2015-07-29 23:16:15 +0100

    Test that CreatedOn field on User is populated.

--- Epsilon/Epsilon.IntegrationTests/Logic/Services/UserPreferenceServiceTest.cs
+++ Epsilon/Epsilon.IntegrationTests/Logic/Services/UserPreferenceServiceTest.cs
@@ -8,11 +8,41 @@ using System.Data.Entity;
 using Ninject;
 using Epsilon.IntegrationTests.BaseFixtures;
 using Epsilon.Logic.Services.Interfaces;
+using Epsilon.Logic.Wrappers.Interfaces;
+using Epsilon.Logic.SqlContext.Interfaces;
+using Epsilon.Logic.Entities;
 
 namespace Epsilon.IntegrationTests.Logic.Services
 {
     public class UserPreferenceServiceTest : BaseIntegrationTestWithRollback
     {
+        [Test]
+        public void CreateUser_DatabasePopulatesCreateOnField()
+        {
+            var container = CreateContainer();
+            var dbContext = container.Get<IEpsilonContext>();
+            var clock = container.Get<IClock>();
+
+            
+            var email = "test@test.com";
+            var user = new User
+            {
+                Email = email,
+                PasswordHash = "PasswordHash",
+                UserName = email
+            };
+            dbContext.Users.Add(user);
+
+            var timeBefore = clock.OffsetNow;
+            dbContext.SaveChanges();
+            var timeAfter = clock.OffsetNow;
+
+            var retrievedUser = DbProbe.Users.Find(user.Id);
+
+            Assert.IsTrue(timeBefore <= retrievedUser.CreatedOn && retrievedUser.CreatedOn <= timeAfter,
+                "The User field CreateOn is not within the expected range.");
+        }
+
         [Test]
         public async Task CreateUserPreference_AndThen_GetUserPreference_AreConsistent()
         {
