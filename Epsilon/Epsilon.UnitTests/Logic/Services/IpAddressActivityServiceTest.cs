7ec653bd4f12f90937f73054d729ab8a33826497
Panos <codiply@gmail.com>
2015-08-18 20:51:20 +0100

    Unit testsfor IpAddressActivityService.

---
+++ Epsilon/Epsilon.UnitTests/Logic/Services/IpAddressActivityServiceTest.cs
@@ -0,0 +1,56 @@
+﻿using Epsilon.Logic.Helpers.Interfaces;
+using Epsilon.Logic.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epsilon.UnitTests.Logic.Services
+{
+    [TestFixture]
+    public class IpAddressActivityServiceTest
+    {
+        [Test]
+        public void RecordLogin_DoesNotThrowButLogsException()
+        {
+            var email = "test@test.com";
+            var ipAddress = "1.2.3.4";
+
+            Exception loggedException = null;
+            var elmahHelper = CreateElmahHelper((ex) => loggedException = ex);
+            var service = new IpAddressActivityService(null, elmahHelper);
+
+            Assert.DoesNotThrow(async () => await service.RecordLogin(email, ipAddress),
+                "Calling RecordLogin should not throw.");
+            Assert.IsNotNull(loggedException,
+                "The exception was not logged.");
+        }
+
+        [Test]
+        public void RecordRegistration_DoesNotThrowButLogsException()
+        {
+            var userId = "user-id";
+            var ipAddress = "1.2.3.4";
+
+            Exception loggedException = null;
+            var elmahHelper = CreateElmahHelper((ex) => loggedException = ex);
+            var service = new IpAddressActivityService(null, elmahHelper);
+
+            Assert.DoesNotThrow(async () => await service.RecordRegistration(userId, ipAddress),
+                "Calling RecordRegistration should not throw.");
+            Assert.IsNotNull(loggedException,
+                "The exception was not logged.");
+        }
+
+        private IElmahHelper CreateElmahHelper(Action<Exception> elmahHelperRaiseHandler)
+        {
+            var mockElmahHelper = new Mock<IElmahHelper>();
+            mockElmahHelper.Setup(x => x.Raise(It.IsAny<Exception>())).Callback(elmahHelperRaiseHandler);
+
+            return mockElmahHelper.Object;
+        }
+    }
+}
