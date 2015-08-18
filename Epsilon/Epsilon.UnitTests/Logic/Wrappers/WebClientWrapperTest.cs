1b4911d3af897799e1cc4eb32f73f9ff4bb18e3e
Panos <codiply@gmail.com>
2015-08-18 12:12:45 +0100

    Finished tests for WebClientWrapper.

--- Epsilon/Epsilon.UnitTests/Logic/Wrappers/WebClientWrapperTest.cs
+++ Epsilon/Epsilon.UnitTests/Logic/Wrappers/WebClientWrapperTest.cs
@@ -38,6 +38,45 @@ namespace Epsilon.UnitTests.Logic.Wrappers
             Assert.IsNotNullOrEmpty(secondResponse.ErrorMessage, "ErrorMessage on second response should not be null or empty.");
         }
 
+        [Test]
+        public async Task DownloadStringTaskAsync_DoesNotThrowButLogsException()
+        {
+            var errorMessage = "error-message";
+            var exception = new Exception(errorMessage);
+
+            Exception exceptionLogged = null;
+            var client = CreateWebClientWrapperWhereTimerThrowsException(exception, x => exceptionLogged = x);
+
+            var timeoutInMilliseconds = 10 * 1000;
+            var url = "http://www.google.com";
+
+            var response = await client.DownloadStringTaskAsync(url, timeoutInMilliseconds);
+
+            Assert.AreEqual(WebClientResponseStatus.Error, response.Status,
+                "Status on response should be error.");
+            Assert.AreEqual(errorMessage, response.ErrorMessage, "ErrorMessage is not the expected.");
+            Assert.IsNotNull(exceptionLogged, "The exception was not logged.");
+            Assert.AreEqual(errorMessage, exceptionLogged.Message,
+                "The Message on the exception logged is not the expected.");
+        }
+
+        [Test]
+        public async Task DownloadStringTaskAsync_TimeoutTest()
+        {
+            var client = CreateWebClientWrapper();
+
+            var timeoutInMilliseconds = 10;
+            var url = "http://www.google.com";
+
+            var response = await client.DownloadStringTaskAsync(url, timeoutInMilliseconds);
+
+            Assert.AreEqual(WebClientResponseStatus.Timeout, response.Status,
+                "Status on response should be error.");
+            Assert.IsNullOrEmpty(response.Response, "Response on response should be emtpy.");
+            Assert.IsNullOrEmpty(response.ErrorMessage, "ErrorMessage on response should be empty.");
+
+        }
+
         private WebClientWrapper CreateWebClientWrapper()
         {
             var timerFactory = new Mock<ITimerFactory>();
@@ -45,6 +84,21 @@ namespace Epsilon.UnitTests.Logic.Wrappers
 
             var elmahHelper = new Mock<IElmahHelper>();
             elmahHelper.Setup(x => x.Raise(It.IsAny<Exception>())).Callback(() => { });
+
+            var webClientWrapper = new WebClientWrapper(timerFactory.Object, elmahHelper.Object);
+
+            return webClientWrapper;
+        }
+
+        private WebClientWrapper CreateWebClientWrapperWhereTimerThrowsException(
+            Exception exception,
+            Action<Exception> elmahHandler)
+        {
+            var timerFactory = new Mock<ITimerFactory>();
+            timerFactory.Setup(x => x.Create()).Returns(() => { throw exception; });
+
+            var elmahHelper = new Mock<IElmahHelper>();
+            elmahHelper.Setup(x => x.Raise(It.IsAny<Exception>())).Callback(elmahHandler);
 
             var webClientWrapper = new WebClientWrapper(timerFactory.Object, elmahHelper.Object);
 
