baa460123ba63923035f3c56814392a66f86884b
Panos <codiply@gmail.com>
2015-09-02 05:38:28 +0100

    Fixed bug with null ratings.

---
+++ Epsilon/Epsilon.Web/Models/ViewModels/Shared/StarRatingReadonlyViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epsilon.Web.Models.ViewModels.Shared
+{
+    public class StarRatingReadonlyViewModel
+    {
+        public byte? Rating { get; set; }
+    }
+}
