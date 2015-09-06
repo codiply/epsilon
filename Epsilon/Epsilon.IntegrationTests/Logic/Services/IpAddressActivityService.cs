2f023ad5602f3c9ea4eb24299835d6af9283e189
Panos <codiply@gmail.com>
2015-09-06 10:27:40 +0100

    Fixed tests.

--- Epsilon/Epsilon.IntegrationTests/Logic/Services/IpAddressActivityService.cs
+++ Epsilon/Epsilon.IntegrationTests/Logic/Services/IpAddressActivityService.cs
@@ -11,6 +11,27 @@ namespace Epsilon.IntegrationTests.Logic.Services
     public class IpAddressActivityService : BaseIntegrationTestWithRollback
     {
         [Test]
+        public async Task RecordWithUserId_Test()
+        {
+            var email = "test@test.com";
+            var ipAddressActivityType = IpAddressActivityType.Registration;
+            var ipAddress = "1.2.3.4";
+
+            var container = CreateContainer();
+
+            var user = await CreateUser(container, email, ipAddress, false);
+
+            var service = container.Get<IIpAddressActivityService>();
+            await service.RecordWithUserId(user.Id, ipAddressActivityType, ipAddress);
+
+            var ipAddressActivity = await DbProbe.IpAddressActivities.SingleOrDefaultAsync(x => x.UserId == user.Id);
+
+            Assert.IsNotNull(ipAddressActivity, "The IpAddressActivity was not found");
+            Assert.AreEqual(ipAddressActivityType, ipAddressActivity.ActivityTypeAsEnum,
+                "The ActivityType was not the expected.");
+        }
+
+        [Test]
         public async Task RecordWithUserEmail_Test()
         {
             var email = "test@test.com";
@@ -22,27 +43,7 @@ namespace Epsilon.IntegrationTests.Logic.Services
             var user = await CreateUser(container, email, ipAddress, false);
 
             var service = container.Get<IIpAddressActivityService>();
-            await service.RecordWithUserEmail(user.Id, ipAddressActivityType, ipAddress);
 
-            var ipAddressActivity = await DbProbe.IpAddressActivities.SingleOrDefaultAsync(x => x.UserId == user.Id);
-
-            Assert.IsNotNull(ipAddressActivity, "The IpAddressActivity was not found");
-            Assert.AreEqual(ipAddressActivityType, ipAddressActivity.ActivityTypeAsEnum,
-                "The ActivityType was not the expected.");
-        }
-
-        [Test]
-        public async Task RecordLoginTest()
-        {
-            var email = "test@test.com";
-            var ipAddressActivityType = IpAddressActivityType.Registration;
-            var ipAddress = "1.2.3.4";
-
-            var container = CreateContainer();
-
-            var user = await CreateUser(container, email, ipAddress, false);
-
-            var service = container.Get<IIpAddressActivityService>();
             await service.RecordWithUserEmail(email, ipAddressActivityType, ipAddress);
 
             var ipAddressActivity = await DbProbe.IpAddressActivities.SingleOrDefaultAsync(x => x.UserId == user.Id);
